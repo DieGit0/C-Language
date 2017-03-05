dd524c552e3011a2cf67cb56b5b8675c152adb5f
DieGit0 <DieGit0@users.noreply.github.com>
2017-03-04 23:35:40 -0300

    Add files via upload

---
+++ CRUD.h
@@ -0,0 +1,13 @@
+#ifndef CRUD_H_INCLUDED
+#define CRUD_H_INCLUDED
+
+
+int OpenFile(char tipo[3]);
+void Entrada();
+void Saida();
+void Procura();
+void Altera();
+char Exclui();
+
+
+#endif // CRUD_H_INCLUDED
